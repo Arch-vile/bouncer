3036cc3b341b69c42522f738a1179239b7362576
Arch-vile <raipe.spamfilter@gmail.com>
2015-10-03 19:56:28 +0300

    angular-fullstack init

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+	// MongoDB connection options
+	mongo: {
+		uri: 'mongodb://bouncer:DJas7IWUY9CD@ds027509.mongolab.com:27509/bouncer'
+	},
+
+	seedDB: true
+};
