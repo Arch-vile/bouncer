0e6da247ed567aac36d96187a8b4830209656a7e
Arch-vile <raipe.spamfilter@gmail.com>
2015-10-10 22:46:05 +0300

    Add log4js logging

--- server/api/dao/bounce/bounce.dao.js
+++ server/api/dao/bounce/bounce.dao.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var db = require('../db');
+var logger = require('log4js').getLogger('bounce.dao');
 
 exports.findByToken = function(token, next) {
 	db.getCollection('bounces').find({
 		token: token
 	}, {}, function(err, doc) {
 		if (err) {
-			console.error("There was a DB problem getting bounce by token [%s]: %s", token, err);
+			logger.error("There was a DB problem getting bounce by token [%s]: %s", token, err);
 			next(new Error("Techinal DB error"));
 		} else if (doc.length > 1) {
 			next(new Error("More then one match"));
