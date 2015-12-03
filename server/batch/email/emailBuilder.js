630b59495b8a0714950cdf26846bf5dab1b53117
Arch-vile <raipe.spamfilter@gmail.com>
2015-12-03 09:55:34 +0200

    EmailBuilder

---
+++ server/batch/email/emailBuilder.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var jade = require('jade');
+var config = require('../../config/environment');
+
+exports.build = function(bounce, next) {
+
+	var options = {
+		pretty: true,
+		topic: bounce.topic,
+		appURL: getAppUrl(),
+		token: bounce.token
+	};
+
+	jade.renderFile('bounceEmailTemplate.jade', options, function(err, html) {
+		if (err) {
+			console.error("Could not render email content: ", err);
+			next(err);
+		} else {
+			next(null, html);
+		}
+	});
+
+};
+
+
+function getAppUrl() {
+	return config.emailLinkTarget;
+}
---
+++ server/batch/email/test/emailBuilderTest.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var should = require('should');
+var assert = require('assert');
+var mockery = require('mockery');
+
+describe('emailBuilder', function() {
+
+	var builder;
+	var configMock = {
+		emailLinkTarget: 'http://theapp.com'
+	};
+
+	before(function() {
+		mockery.enable({
+			useCleanCache: true
+		});
+		mockery.enable();
+		mockery.warnOnUnregistered(false);
+		mockery.registerMock('../../config/environment', configMock);
+		builder = require('../emailBuilder');
+	});
+
+	describe('build', function() {
+
+		var bounce = {
+			topic: 'this is the topic',
+			token: 'theToken'
+		};
+
+		it('should contain bounce topic', function(done) {
+			builder.build(bounce, function(err, content) {
+				assert(content.indexOf('this is the topic') !== -1);
+				done();
+			});
+		});
+
+		it('should contain links to defer the bounce', function(done) {
+			builder.build(bounce, function(err, content) {
+
+				// Defer two hours
+				assert(content.indexOf('http://theapp.com/defer/theToken?amount=2&amp;units=hours') !== -1);
+
+				// Defer four hours
+				assert(content.indexOf('http://theapp.com/defer/theToken?amount=4&amp;units=hours') !== -1);
+
+				// Defer one day
+				assert(content.indexOf('http://theapp.com/defer/theToken?amount=1&amp;units=days') !== -1);
+
+				// Defer two days
+				assert(content.indexOf('http://theapp.com/defer/theToken?amount=2&amp;units=days') !== -1);
+
+				// Defer one week
+				assert(content.indexOf('http://theapp.com/defer/theToken?amount=1&amp;units=weeks') !== -1);
+
+				// Defer two weeks
+				assert(content.indexOf('http://theapp.com/defer/theToken?amount=2&amp;units=weeks') !== -1);
+
+				// Defer one month
+				assert(content.indexOf('http://theapp.com/defer/theToken?amount=1&amp;units=months') !== -1);
+
+				// Defer two hours
+				assert(content.indexOf('http://theapp.com/defer/theToken?amount=2&amp;units=months') !== -1);
+
+
+				done();
+			});
+		});
+
+	});
+
+});
