939c51ef9b83a4846a0792f13fecd24f96b5c432
Arch-vile <raipe.spamfilter@gmail.com>
2015-12-05 12:12:42 +0200

    Remove Thing

--- server/routes.js
+++ server/routes.js
@@ -10,7 +10,6 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/bounce', require('./api/endPoint/bounce'));
 
   // All undefined asset or api routes should return a 404
