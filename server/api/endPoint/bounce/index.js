2ff214dcc74fce7b0d5a7bcc3dbc64ac14da2e28
Arch-vile <raipe.spamfilter@gmail.com>
2015-12-06 11:48:58 +0200

    FIX: Add missing route for deferring

--- server/api/endPoint/bounce/index.js
+++ server/api/endPoint/bounce/index.js
@@ -5,8 +5,9 @@ var controller = require('./bounceEndpoint');
 
 var router = express.Router();
 
-router.get('/', controller.show);
+
 router.get('/:token', controller.show);
 router.post('/new', controller.create);
+router.put('/defer', controller.defer);
 
 module.exports = router;
