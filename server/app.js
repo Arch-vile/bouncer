c826fbf2609da7ce28505f5a95bdb842369a18f8
Arch-vile <raipe.spamfilter@gmail.com>
2015-12-28 11:29:50 +0200

    Fix mongoskin dependency issue

--- server/app.js
+++ server/app.js
@@ -8,19 +8,13 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
-var mongoose = require('mongoose');
 var config = require('./config/environment');
 
 var log4js = require('log4js');
 log4js.loadAppender('file');
 log4js.addAppender(log4js.appenders.file('app.log'));
 
-// Connect to database
-mongoose.connect(config.mongo.uri, config.mongo.options);
-mongoose.connection.on('error', function(err) {
-	console.error('MongoDB connection error: ' + err);
-	process.exit(-1);
-});
+
 // Populate DB with sample data
 if (config.seedDB) {
 	require('./config/seed');
