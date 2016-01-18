fdd777128cfc280ebd2168e72c377398ef7ce272
Arch-vile <raipe.spamfilter@gmail.com>
2016-01-18 19:23:06 +0200

    Change to use logger instead of console out

--- server/batch/email/emailBatch.js
+++ server/batch/email/emailBatch.js
@@ -6,27 +6,27 @@ var builder = require('./emailBuilder');
 var logger = require('log4js').getLogger('emailBatch');
 
 exports.run = function() {
-	console.info('Sending reminders');
+	logger.info('Running email batch job');
 	dao.getPending(function(err, pending) {
 		if (err) {
 			// TODO: log error and alert
 		} else {
 			logger.info('Sending ' + pending.length + ' reminders');
 			pending.forEach(function(bounce) { 
+				logger.info('Sending reminder with topic: ' + bounce.topic);
 				builder.build(bounce, function(err, html) {
 					if (err) {
-						// TODO: log error and alert
+						logger.error("Error building email content: " + err);
 					} else {
-						logger.trace('Email body: ' + html);
 						emailService.send(bounce.topic, html, bounce.email, function(err) {
 							if (err) {
-								// TODO: log error and alert
-								console.error("Error sending email: " + err);
+								logger.error("Error sending email: " + err);
 							} else {
-
 								dao.deactivate(bounce, function(err, updated) {
 									if (err) {
-										// TODO: log error and alert
+										logger.error("Error deactivating bounce: " + err);
+									} else {
+										logger.info('Reminder sent successfully');
 									}
 								});
 							}
